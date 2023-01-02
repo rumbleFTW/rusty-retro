7fc7b0d7225b240012a1817b5dd4287254497e0a
rumbleFTW <007rajdeepghosh@gmail.com>
2023-01-02 15:13:45 +0530

    Finished chip8

--- nes/target/debug/deps/nes-7650cac3937a72be.d
+++
@@ -1,5 +0,0 @@
-/home/rumbleftw/Documents/Codes/rusty-retro/nes/target/debug/deps/nes-7650cac3937a72be.rmeta: src/main.rs
-
-/home/rumbleftw/Documents/Codes/rusty-retro/nes/target/debug/deps/nes-7650cac3937a72be.d: src/main.rs
-
-src/main.rs:
