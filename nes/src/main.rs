1214cb522823577cfb3550cd616b0a196b50b5c5
Rajdeep Ghosh <007rajdeepghosh@gmail.com>
2023-06-20 22:44:46 +0530

    Added ROM loading

--- nes/src/main.rs
+++ nes/src/main.rs
@@ -1,10 +1,36 @@
 mod nes;
-// use std::fs;
-// use std::io::Read;
+use std::env;
+use std::fs;
+use std::io::Read;
 
 fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let mut rom_path = "default";
+    let mut debug = false;
+
+    for arg in args.iter() {
+        if arg.starts_with("--rom=") {
+            rom_path = &arg[6..]
+        }
+        if arg == "-d" {
+            debug = true;
+        }
+    }
+
     let mut ns = nes::Nes::new();
-    ns.debug();
-    ns.emulate_cycle();
-    println!("Hello world!");
+    if debug == true {
+        ns.debug();
+    }
+
+    if rom_path == "default" {
+        // c8.memory.load_program(&C8_ROM);
+    } else {
+        let mut f = fs::File::open(&rom_path).expect("no file found");
+        let metadata = fs::metadata(&rom_path).expect("unable to read metadata");
+        let mut buffer = vec![0; metadata.len() as usize];
+        f.read(&mut buffer).expect("buffer overflow");
+        ns.cpu.program_counter = 0xC000;
+        ns.memory.load_program(&buffer);
+    }
 }
--- nes/src/nes/memory.rs
+++ nes/src/nes/memory.rs
@@ -4,6 +4,15 @@ pub struct Memory {
 }
 
 impl Memory {
+    pub fn load_program(&mut self, program: &[u8]) {
+        for i in 16..program.len() {
+            println!("{:#X}", 0x8000 + i);
+            self.primary_memory[0x8000 + i] = program[i];
+        }
+    }
+}
+
+impl Memory {
     pub fn new() -> Memory {
         return Memory {
             primary_memory: [0x0; 64 * 1024],
